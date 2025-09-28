1a329e9837d4339b2afc061f907986ceb68abc40
Aniket Singh <129079574+aniketsingh1023@users.noreply.github.com>
2025-09-28 16:35:39 +0000

    Added dsome basic console log  and var and loop question for revision

---
+++ script.js
@@ -0,0 +1,142 @@
+//console.log("aniket","love to game")
+
+//console.log(45*2-10)
+
+
+
+
+//use console.log to print year
+//const newDate = new Date(); --> cool thing 
+//console.log(newDate.getFullYear())
+
+
+//create two variables, concatenate and log them 
+/*   var first = "aniket";
+  var last="Singh";
+console.log(first+ " " + last)  */
+
+
+
+//---- track the value of a variable by loggin it before and after updating 
+
+/* var a = 10;
+console.log(a)
+ a =46;
+console.log(a) */
+
+
+//--Use console.error( to stimulate an message
+
+/* console.error("Something went wrong") */
+
+//---- print the type of variable holding the value true
+
+/* var a = true;
+console.log(typeof(a)) */
+
+
+
+// -- create a variable holding your  age and log whether is greater than 18.
+
+
+/* var age = 21;
+
+if(age > 18){
+    console.log(true)
+} */
+
+
+
+
+
+
+//---------- log the result of 100/0 = infinity
+
+/* console.log(100/0) */
+
+
+
+
+
+
+// 2. Variable and Type Of variables
+
+
+
+//------- declare a variable using let and log its value
+/* let aa = 12;
+console.log(aa) */
+
+
+
+
+//---create a constant to store the value of pi and log it 
+/* const pieValue = Math.PI
+console.log(pieValue) */
+
+
+
+//--Reassign a value to a variable declared with let and log the result
+/* let value = 12
+value = 23
+console.log(value) */
+
+
+
+//---check the typeof null and log it = object 
+/* console.log(typeof(null)) */
+
+
+//-- create a variable with a number as a string and log its type
+/* var a = "12"
+console.log(typeof(a)) */
+
+
+
+//--create three variables with a number as a string ,numberand boolena and log their values
+/* let aa = " hey ", bb = 13, cc = true
+console.log(aa ,  bb ,  cc) */
+
+
+
+//--- declare a variable without assigning its value and log its type
+/* var aa;
+console.log(aa) */
+
+
+
+
+//--- create a variable without assigning a value and log its type
+/* var aa = undefined;
+console.log(typeof(aa)) */
+
+
+
+//--use const to create an array .try reassigning the array and observe the error 
+/*  const thisisArray = [1,2,3,4];
+ console.log(thisisArray)
+ thisisArray = [2,34,4,6];
+ console.log(thisisArray) */
+ /* script.js:114 (4) [1, 2, 3, 4]
+script.js:115 Uncaught TypeError: Assignment to constant variable.
+    at script.js:115:14
+(anonymous) @ script.js:115
+ */
+
+
+
+//3 . Question on loops 
+
+//write a loop to print numbers from 1 to 50
+
+/* for(var i = 1; i<51;i++){
+    console.log(i)
+} */
+
+
+
+
+
+//------- use a while loop to sum the number from 1 to 10 
+
+
